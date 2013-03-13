4b0865a4f0cce60a8ad24f8e8e75d12885aae15b
unknown <mortenmnc@hotmail.com>
2013-03-13 15:34:24 +0100

    Update namespace for classes

--- VocApp/VocApp.Model/Word.cs
+++ VocApp/VocApp.Model/Word.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VocabularyGUI {
+namespace VocApp.Model {
     class Word {
         Word[] wrong_ans = new Word[4];
 
