e4e8967ed29588ec119bd4ea6d90061cc27f88f3
Patrik Reppien <reppien@gmail.com>
2013-03-13 15:01:43 +0100

    Added Reader classes

---
+++ VocApp/VocApp.Model/PdfReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocApp.Model {
+    public class PdfReader : Reader {
+
+        protected override string GetString(string filepath) {
+            ISet<Word> result = new HashSet<Word>();
+            return result;
+        }
+    }
+}
---
+++ VocApp/VocApp.Model/Reader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocApp.Model {
+    public class Reader {
+
+        private char[] specialcharacters = { '\n', ' ', '.', ',', '!', '?', '(', ')' };
+
+        protected abstract string GetString(string name);
+
+        public ISet<Word> Read(string source) {
+            ISet<Word> result = new HashSet<Word>();
+            string readstring = GetString(source);
+            StringBuilder stringbuilder = new StringBuilder(20);
+            foreach (char c in readstring) {
+                if (NotSpecialCharacter(c)) {
+                    stringbuilder.Append(c);
+                }
+            }
+            return result;
+        }
+
+        private bool NotSpecialCharacter(char c) {
+            foreach (char sp in specialcharacters) {
+                if (sp == c) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
